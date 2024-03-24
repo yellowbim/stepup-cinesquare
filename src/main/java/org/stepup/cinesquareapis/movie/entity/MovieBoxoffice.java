db7c845a7a3c659a97ecbdad368756762dec0e6d
ssoomnyb <ssoomnyb@gmail.com>
2024-03-25 04:28:43 +0900

    feat: Add get movie boxoffice api

---
+++ src/main/java/org/stepup/cinesquareapis/movie/entity/MovieBoxoffice.java
@@ -0,0 +1,49 @@
+package org.stepup.cinesquareapis.movie.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "tb_movie_boxoffice")
+public class MovieBoxoffice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id; // 의미X
+
+    @Column(length = 2, nullable = false)
+    private Integer rank;
+
+    @Column(nullable = false)
+    private Integer movieId;
+
+    @Column(length = 2, nullable = false)
+    private Integer yearWeek;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column
+    private Integer showCount;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime created;
+
+    @UpdateTimestamp
+    @Column
+    private LocalDateTime updated;
+}
