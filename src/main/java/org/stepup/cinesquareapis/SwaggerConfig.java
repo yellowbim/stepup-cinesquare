1b25b11151f8633a395a91b1a94857484ad00a2c
ssoomnyb <ssoomnyb@gmail.com>
2024-03-27 04:59:07 +0900

    feat: Apply key of returned json to be snake_case in Swagger ui

--- src/main/java/org/stepup/cinesquareapis/SwaggerConfig.java
+++ src/main/java/org/stepup/cinesquareapis/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package org.stepup.cinesquareapis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.jackson.ModelResolver;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -20,5 +23,10 @@ public class SwaggerConfig {
                 .title("CINESQUARE API") // API의 제목
                 .description("Let's practice Swagger UI") // API에 대한 설명
                 .version("1.0.0"); // API의 버전
+    }
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
     }
 }
